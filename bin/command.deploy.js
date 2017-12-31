9c3cd2231d9216ce95bca12578f80c5526fc3f25
Busyrich <rich@busyrich.com>
2017-12-30 20:17:13 -0700

    Moved the deploy logic to a controller and updated the deploy command to use the new controller. Resolves #1

--- bin/command.deploy.js
+++ bin/command.deploy.js
@@ -10,37 +10,6 @@ module.exports = function(cli, project) {
   var pajo = require('path').join,
       utils = require(pajo(__dirname, '..', 'lib',
         'controllers', 'controller.utils.js'));
-      rexoFunctionsPath = pajo(__dirname, '..', 
-        'deployment', 'cloud_functions');
-  
-  var deployLambda = function(deploymentFolder, npmModule, functionName, callback) {
-    var lambdaPath = pajo(rexoFunctionsPath, 'aws', deploymentFolder);
-
-    utils.async.waterfall([
-      utils.async.apply(utils.fse.rmrf, utils.deployTmp),
-      utils.async.apply(utils.fse.copyRecursive, lambdaPath, utils.deployTmp),
-      utils.async.apply(utils.npmLCopyLocal, npmModule, utils.deployTmp),
-      utils.async.apply(utils.zipFolder, utils.deployTmp, functionName),
-      function(zipFile, cb) {
-        var lambda = new utils.AWS.Lambda({
-          region: project.PLATFORM_REGION,
-          accessKeyId: project.AWS_LAMBDA_UPDATER_KEY,
-          secretAccessKey: project.AWS_LAMBDA_UPDATER_SECRET
-        });
-        
-        lambda.updateFunctionCode({
-          FunctionName: functionName,
-          ZipFile: utils.fs.readFileSync(zipFile)
-        }, cb);
-      }
-    ], function(err) {
-      if(!err) {
-        utils.log('Function Deployed.'.green);
-      }
-
-      callback(err);
-    });
-  };
 
   //Provides a simple countdown in the console
   var countdown = function(seconds, message, callback) {
@@ -75,11 +44,9 @@ module.exports = function(cli, project) {
       process.env.NODE_ENV = env;
       project.load();
 
-      var templateBucket = project.TEMPLATE_STORAGE + 
-            '/' + process.env.REXO_PROJECT,
-          cdnBucket = project.CDN_STORAGE +
-            '/' + process.env.REXO_PROJECT,
-            actions = [];
+      var deploy = require(pajo(__dirname,
+        '..', 'lib', 'controllers',
+        'controller.deploy.js'))(project);
       
       utils.printHeader('Starting Rexo Deploy', true);
       utils.log('Selected Environment: ', env.cyan);
@@ -93,26 +60,25 @@ module.exports = function(cli, project) {
       actions.push(function(callback) {
         utils.printHeader('Syncing Your Templates');
         utils.log('Templates Local Path: ', project.env.TEMPLATE_PATH.magenta);
-        utils.log('Templates Cloud Storage Path: ', templateBucket.magenta);
+        utils.log('Templates Cloud Storage Path: ', 
+          deploy.templateBucket.magenta);
+
         callback(null);
       });
 
-      actions.push(utils.async.apply(utils.awsSync, 
-        project.env.TEMPLATE_PATH, templateBucket));
+      actions.push(utils.async.apply(deploy.templates));
       
       actions.push(function(callback) {
         utils.log('Templates Updated.'.green);
         utils.printHeader('Syncing Your CDN Files');
         utils.log('CDN Local Path: ', project.env.CDN_PATH.magenta);
-        utils.log('CDN Cloud Storage Path: ', cdnBucket.magenta);
+        utils.log('CDN Cloud Storage Path: ', 
+          deploy.cdnBucket.magenta);
+
         callback(null);
       });
 
-      actions.push(utils.async.apply(utils.gulpTask, 
-        'build', project.env));
-
-      actions.push(utils.async.apply(utils.awsSync, 
-        project.env.CDN_PATH, cdnBucket));
+      actions.push(utils.async.apply(deploy.cdn));
       
       actions.push(function(callback) {
         utils.log('CDN Updated.'.green);
@@ -125,11 +91,8 @@ module.exports = function(cli, project) {
           callback(null);
         });
 
-        actions.push(utils.async.apply(deployLambda,
-          'api', 'rexo-api-lambda', project.API_FUNCTION));
-        
-        actions.push(utils.async.apply(deployLambda,
-          'frontend', 'rexo-frontend-lambda', project.FRONTEND_FUNCTION));
+        actions.push(utils.async.apply(deploy.api));
+        actions.push(utils.async.apply(deploy.frontend));
       }
 
       utils.async.waterfall(actions, function(err) {
