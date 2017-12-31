a3981957bb6fdb74d68ddcc50f773f41c464f5ef
Busyrich <rich@busyrich.com>
2017-12-30 20:31:58 -0700

    Moved the countdown helper to utils.

--- bin/command.deploy.js
+++ bin/command.deploy.js
@@ -10,31 +10,6 @@ module.exports = function(cli, project) {
   var pajo = require('path').join,
       utils = require(pajo(__dirname, '..', 'lib',
         'controllers', 'controller.utils.js'));
-
-  //Provides a simple countdown in the console
-  var countdown = function(seconds, message, callback) {
-    var ticks = 1, countdown = seconds;
-
-    process.stdout.write(message + ' ' + countdown);
-    var timer = setInterval(function() {
-      if(ticks > 0 && ticks % 4 === 0) {
-        countdown--;
-        if(countdown === 0) {
-          process.stdout.write('GO!');
-        } else {
-          process.stdout.write(countdown + '');
-        }
-      } else if(countdown === 0) {
-        process.stdout.write('\n');
-        clearTimeout(timer);
-        callback(null);
-      } else {
-        process.stdout.write('.');
-      }
-
-      ticks++;
-    }, 250);
-  };
   
   cli.command('deploy <env>')
     .description('deploys a Rexo project under the environment given')
@@ -55,7 +30,8 @@ module.exports = function(cli, project) {
       //This gives the user 5 seconds to cancel the deploy
       //just in case the environment is wrong or they ran the
       //command by accident.
-      actions.push(utils.async.apply(countdown, 5, 'Deploying Project in'));
+      actions.push(utils.async.apply(utils.countdown, 
+        5, 'Deploying Project in'));
 
       actions.push(function(callback) {
         utils.printHeader('Syncing Your Templates');
